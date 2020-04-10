ec09479bb7c9f7a690e19c76dbbe845422e9db09
VenkateswaranCGI <31267420+VenkateswaranCGI@users.noreply.github.com>
2020-04-10 08:15:22 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -6,28 +6,5 @@
 def call(String buildStatus = 'STARTED') {
   // build status of null means successful
   echo 'test'
-  buildStatus =  buildStatus ?: 'SUCCESSFUL'
-
-  // Default values
-  def color = 'RED'
-  def colorCode = '#FF0000'
-  def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
-  def summary = "${subject} (${env.BUILD_URL})"
-  def details = """<p>${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
-    <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>"""
-
-  // Override default values based on build status
-  if (buildStatus == 'STARTED') {
-    color = 'YELLOW'
-    colorCode = '#FFFF00'
-  } else if (buildStatus == 'SUCCESSFUL') {
-    color = 'GREEN'
-    colorCode = '#00FF00'
-  }
-
-  // Send notifications
-  //slackSend (color: colorCode, message: summary)
-
-  //hipchatSend (color: color, notify: true, message: summary)
 
 }
