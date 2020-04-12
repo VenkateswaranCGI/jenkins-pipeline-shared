9099dac45e591e85d3ee5516b68ed9fd68caed3f
VenkateswaranCGI <31267420+VenkateswaranCGI@users.noreply.github.com>
2020-04-12 20:14:28 +0530

    Update Build.groovy

--- src/com/org/jenkins/stages/Build.groovy
+++ src/com/org/jenkins/stages/Build.groovy
@@ -4,7 +4,8 @@ package com.org.jenkins.stages.foo
 class Build {
   
   def mvnbuild(script) {
-    def buildcommand = 'mvn -B clean install -DskipTests'
+    //def buildcommand = 'mvn -B clean install -DskipTests'
+    def buildcommand = 'mvn clean package'
     println('Starting Maven Build')
     script.sh buildcommand
     println('Completed Maven Build')
