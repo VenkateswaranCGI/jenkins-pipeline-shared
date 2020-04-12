213e3778f45ba080f8613ce209c845ea224758e7
VenkateswaranCGI <31267420+VenkateswaranCGI@users.noreply.github.com>
2020-04-12 10:46:33 +0530

    Update utils.groovy

--- src/org/foo/utils.groovy
+++ src/org/foo/utils.groovy
@@ -1,12 +1,6 @@
 package org.foo
 class utils implements Serializable {
-  
-  private final def script
-  
-  utils(def script) {
-    this.script = script
-  }
-  
+
   void mvn() {
     //script.sh 'mvn -version'
     echo "hello"
