e8b0bbf3dd10b2c2940f3c3e37521621bd9a0b6b
VenkateswaranCGI <31267420+VenkateswaranCGI@users.noreply.github.com>
2020-04-12 21:14:56 +0530

    Update Analysis.groovy

--- src/com/org/jenkins/stages/Analysis.groovy
+++ src/com/org/jenkins/stages/Analysis.groovy
@@ -4,9 +4,9 @@ package com.org.jenkins.stages.foo
 class Analysis {
   
   def sonaranalysis(script) {
-    //def buildcommand = 'mvn package'
+    //def analysiscommand = "/opt/sonar-scanner/bin/sonar-scanner"
     println('Starting Code Analysis')
-    //script.sh buildcommand
+    //script.sh analysiscommand
     println('Completed Code Analysis')
   }
 }
