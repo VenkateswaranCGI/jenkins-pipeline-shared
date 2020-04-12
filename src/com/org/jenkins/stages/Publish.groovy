e1b435a90e1686f128e22ad25142fb3c6e41ab13
VenkateswaranCGI <31267420+VenkateswaranCGI@users.noreply.github.com>
2020-04-12 20:21:10 +0530

    Update Publish.groovy

--- src/com/org/jenkins/stages/Publish.groovy
+++ src/com/org/jenkins/stages/Publish.groovy
@@ -1,1 +1,12 @@
+#!/usr/bin/env groovy
 
+package com.org.jenkins.stages.foo
+class Publish {
+  
+  def publishfile(script) {    
+    println('Starting Publish')
+    //script.sh "docker build -t alsac-alsac-poc.jfrog.io/artifact ."
+    //script.sh "docker tag alsac-alsac-poc.jfrog.io/artifact:latest ."
+    println('Completed Publish')
+  }
+}
